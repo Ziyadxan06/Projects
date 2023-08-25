b73d1e6b3ec59e707cce409bd62cc9258805de69
Ziyadxan Dovletov <ziyadxan.dovletov06@gmail.com>
2023-08-25 06:25:37 -0700

    COM_2 Task-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+function addElement(e){
+  const lists = document.querySelector("ul");
+  let input = document.querySelector(".todo-input");
+  e.preventDefault();
+  
+    if(input.value){
+      lists.innerHTML +=`
+      <li>
+        <a>${input.value}</a>
+        <i class="fa-solid fa-trash"></i>
+      </li>
+      `
+    }
+
+    else{
+        alert("Please enter a task");
+    }
+
+    input.value = "";
+
+    deleteItems();
+}
+
+function deleteItems(){
+    const listItemicon = document.querySelectorAll("i");
+
+    listItemicon?.forEach((elem) => {
+        elem?.addEventListener('click', () => {
+                elem.parentElement.remove();
+        })
+        
+    })
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-container">
+        <form>
+            <h1>ToDo</h1>
+            <input placeholder="add todo" className="todo-input" type="text" />
+            <button onClick={addElement} className="btn">Add</button>
+        </form>
+        <ul className="todo-lists">
+        </ul>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
